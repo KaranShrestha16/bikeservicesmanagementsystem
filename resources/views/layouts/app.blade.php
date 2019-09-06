fa8d550c24b29a02a81f2e7b7ffc956151ff9eb7
KARANSHRESTHA123 <48661099+KARANSHRESTHA123@users.noreply.github.com>
2019-09-06 10:52:26 +0545

    Add 2 extra options in the drop down list

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,17 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
+
+                                    <a class="dropdown-item" href="#">
+                                       Update Profile
+                                    </a>
+
+                                    <a class="dropdown-item" href="#">
+                                       Change Password
+                                    </a>
+
                                 </div>
+
                             </li>
                         @endguest
                     </ul>
