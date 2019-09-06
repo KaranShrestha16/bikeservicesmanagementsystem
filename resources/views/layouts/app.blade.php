8b1e18560534513e416c54991446d83ffd10f653
KARANSHRESTHA123 <48661099+KARANSHRESTHA123@users.noreply.github.com>
2019-09-06 10:44:32 +0545

    Create User Dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,8 +73,72 @@
         </nav>
 
         <main class="py-4">
+        
+        @Auth
+
+            <div class="container">
+
+            @if(session()->has('success'))
+
+                <div class="alert alert-success">
+                    {{ session()->get('success') }}
+                </div>
+
+            @endif
+
+            @if(session()->has('error'))
+
+                <div class="alert alert-danger">
+                    {{ session()->get('error') }}
+                </div>
+
+            @endif
+
+                <div class="row">
+                    <div class="col-md-4">
+
+                        <ul class="list-group">
+
+                            <li class="list-group-item">
+                                <a href="#">Booking Form</a>
+                            </li>
+                            
+                            <li class="list-group-item">
+                                <a href="#">Booking History</a>
+                            </li>
+                            
+                            <li class="list-group-item">
+                                <a href="#s">Extra</a>
+                            </li>
+
+                        </ul>
+
+                        <ul class="list-group mt-5">
+
+                            <li class="list-group-item">
+                                <a href="#">Inquiry</a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+
+                    <div class="col-md-8">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+
+        @else
+
             @yield('content')
+
+        @endauth
+
+            
         </main>
     </div>
+
 </body>
 </html>
+                    
