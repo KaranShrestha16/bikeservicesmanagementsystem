6550f2cdf9e1116deb54f2ee5110a927ca5bef7f
KARANSHRESTHA123 <48661099+KARANSHRESTHA123@users.noreply.github.com>
2019-09-07 14:36:23 +0545

    Create Ui

--- resources/views/admin_home.blade.php
+++ resources/views/admin_home.blade.php
@@ -2,7 +2,29 @@
 
     @if(auth()->user()->isAdmin())
 
-        Welcome Admin
+    @extends('layouts.app')
+
+        @section('content')
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">Dashboard</div>
+
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            Welcome, ADMIN
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endsection
 
     @else
 
