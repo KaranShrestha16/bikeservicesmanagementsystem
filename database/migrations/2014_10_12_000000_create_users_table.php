1d6232efcc1a1a7d16fa3305577ecbd461cce85f
KARANSHRESTHA123 <48661099+KARANSHRESTHA123@users.noreply.github.com>
2019-09-07 13:10:55 +0545

    Add user role and create seeder for admin data entry.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['normal','admin'])->default('normal');
             $table->integer('phoneno');
             $table->string('address');
             $table->rememberToken();
