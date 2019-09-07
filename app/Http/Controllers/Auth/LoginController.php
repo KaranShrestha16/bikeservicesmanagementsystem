700fe71ee41732a60190df30116b9f5aed0be011
KARANSHRESTHA123 <48661099+KARANSHRESTHA123@users.noreply.github.com>
2019-09-07 14:22:20 +0545

    Admin Dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -33,7 +33,7 @@ class LoginController extends Controller
     {
         if(auth()->user()->isAdmin())
         {
-            return '/admin_home';
+            return '/admin';
         } else {
             return '/home';
         }
