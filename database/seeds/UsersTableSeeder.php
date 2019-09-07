0d4d82a728772975d8846e6f49ce48b69697f3c2
KARANSHRESTHA123 <48661099+KARANSHRESTHA123@users.noreply.github.com>
2019-09-07 13:14:58 +0545

    Create function in seeder to add admin details.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +13,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::where('email', 'asd@gmail.com')->first();
+
+        if(!$user)
+        {
+            User::create([
+                'name' => 'Karan Shrestha',
+                'email' => 'asd@gmail.com',
+                'password' => Hash::make('asd12345'),
+                'role' => 'admin'
+            ]);
+        }
     }
 }
